03efb8b0fee067ba77610f41cfacccc92bbcc8b2
Christoph <schmitzchristoph7@gmail.com>
2022-10-02 10:59:50 +0200

    Main menu added

--- app/src/main/java/de/christoph/prinzkoch/MainActivity.kt
+++ app/src/main/java/de/christoph/prinzkoch/MainActivity.kt
@@ -2,10 +2,45 @@ package de.christoph.prinzkoch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpActionBar()
     }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        toolbar_main_activity.setNavigationOnClickListener {
+            onNavigationClick()
+        }
+    }
+
+    private fun onNavigationClick() {
+        toggleDrawer()
+    }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START))
+            drawer_layout.closeDrawer(GravityCompat.START)
+    }
+
+    private fun toggleDrawer() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+
 }
